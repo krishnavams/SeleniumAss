d4a83b9aa6c460b53ab390b257fd721482fe6dd5
krishnavams <35889193+krishnavams@users.noreply.github.com>
2022-02-09 09:52:32 +0530

    today

---
+++ testingEnd/src/testingEnd/fastInternet.java
@@ -0,0 +1,35 @@
+package testingEnd;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class fastInternet {
+
+	public static void main(String[] args)  {
+		apps.path();
+		WebDriver d1 =new ChromeDriver();
+		d1.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		d1.get("https://fast.com/");
+		
+		
+		WebDriverWait w1 = new WebDriverWait(d1, 60);
+		w1.until(ExpectedConditions.textToBe(By.id("your-speed-message"),"Your Internet speed is"));
+		String internet = d1.findElement(By.id("your-speed-message")).getText();
+		String speed = d1.findElement(By.id("speed-value")).getText();
+		System.out.println(internet+" "+speed+" Mbps");
+		d1.get("https://www.speedtest.net/");
+		d1.findElement(By.className("start-text")).click();
+		//Thread.sleep(10000);
+		d1.findElement(By.xpath("//li[.='Much better']"));
+		speed=d1.findElement(By.xpath("(//div[@class='result-data u-align-left'])[2]//span")).getText();
+		String down = d1.findElement(By.xpath("(//div[@class='result-data u-align-left'])[3]//span")).getText();
+		System.out.println(internet+" "+speed+" Mbps and Download speed is "+down+" Mbps");
+		d1.close();
+		}
+
+}
