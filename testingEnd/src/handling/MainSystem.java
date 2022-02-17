30b2953de92b5fd18e6ad9d41dacf102d7ad634d
krishnavams <35889193+krishnavams@users.noreply.github.com>
2022-02-17 22:47:06 +0530

    taday taday

--- testingEnd/src/handling/MainSystem.java
+++ testingEnd/src/handling/MainSystem.java
@@ -1,29 +1,37 @@
 package handling;
 
 public class MainSystem {
-	static String username="gehexin499@bepureme.com";
-	static String Password="Abcd#257";
-	static String url="https://demo.actitime.com/login.do";
+	private static String username="gehexin499@bepureme.com";
+	private static String Password="Abcd#257";
+	private static String url="https://demo.actitime.com/login.do";
 
-	public static void path() {
+	public void path() {
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 		System.setProperty("webdriver.msedge.driver", "./driver/msedgedriver.exe");
 	}
 
-	public static String getStr() {
+	public String getPassword() {
 		return Password;
 	}
 	
-	public static String getUsername() {
+	public String getUsername() {
 		return username;
 	}
 
-	public static void setUsername(String username) {
+	public void setUsername(String username) {
 		MainSystem.username = username;
 	}
 
-	public static void setStr(String str) {
+	public void setPassword(String str) {
 		MainSystem.Password = str;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		MainSystem.url = url;
 	}
 	
 }
--- testingEnd/src/handling/resume.java
+++ testingEnd/src/handling/resume.java
@@ -8,18 +8,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+/**
+ * 
+ * @author VAMSI
+ *
+ */
+
 public class resume {
 	static WebDriver d1;
 	static File file;
-	static String url="https://www.naukri.com/";
+	static String url = "https://www.naukri.com/";
+	static MainSystem mainSystem;
+
 	public static void main(String[] args) throws Exception {
-		MainSystem.path();
-		file=new File("./support/Fake Resume.pdf");
-		d1=new ChromeDriver();
+		mainSystem = new MainSystem();
+		mainSystem.path();
+		file = new File("./support/Fake Resume.pdf");
+		d1 = new ChromeDriver();
 		d1.get(url);
 		login();
 		uploadFile(file);
 	}
+
+	@SuppressWarnings("deprecation")
 	public static void uploadFile(File file) {
 		d1.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		d1.findElement(By.xpath("(//div[.='UPDATE PROFILE'])[1]")).click();
@@ -31,19 +42,25 @@ public class resume {
 		d1.findElement(By.linkText("CANCEL")).click();
 		System.out.println("upload pass");
 	}
-	public static void login() throws Exception  {
+
+	@SuppressWarnings("deprecation")
+	public static void login() throws Exception {
 		d1.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		d1.manage().deleteAllCookies();
 		d1.findElement(By.xpath("//div[.='Login']")).click();
-		d1.findElement(By.xpath("//label[contains(.,'Email ID / Username')]/../input")).sendKeys(MainSystem.username);
-		d1.findElement(By.xpath("//label[contains(.,'Password')]/../input")).sendKeys(MainSystem.Password);
+		d1.findElement(By.xpath("//label[contains(.,'Email ID / Username')]/../input"))
+				.sendKeys(mainSystem.getUsername());
+		d1.findElement(By.xpath("//label[contains(.,'Password')]/../input")).sendKeys(mainSystem.getPassword());
 		Thread.sleep(2000);
 		d1.findElement(By.xpath("(//button[contains(.,'Login')])[1]")).click();
-		try{
+		try {
 			d1.findElement(By.xpath("//button[text()='SKIP AND CONTINUE']")).click();
-		}catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Login Pass");
-			try{
+			try {
 				d1.findElement(By.className("crossIcon chatBot chatBot-ic-cross")).click();
-			}catch(Exception e1) {
-				}}}}
+			} catch (Exception e1) {
+			}
+		}
+	}
+}
